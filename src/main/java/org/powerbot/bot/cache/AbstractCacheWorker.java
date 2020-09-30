52f0fabd576c59861076a722135a53090e06c122
omijagtap <72138819+omijagtap@users.noreply.github.com>
2020-09-30 17:48:46 +0530

    Goodlook

--- src/main/java/org/powerbot/bot/cache/AbstractCacheWorker.java
+++ src/main/java/org/powerbot/bot/cache/AbstractCacheWorker.java
@@ -1,5 +1,5 @@
 package org.powerbot.bot.cache;
-
+//amazing code plzz add this
 public abstract class AbstractCacheWorker {
 
 	public abstract Block getBlock(final int tree_index, final int block);
